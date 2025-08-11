945eac9a5bb9235b3b48e9f1d5f5de7c385515f2
distriBUTEURdu73 <distriBUTEURdu73@gmail.com>
2025-08-11 06:42:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ def home():
     return "Bot en ligne !"
 
 def run_flask():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run_flask)
